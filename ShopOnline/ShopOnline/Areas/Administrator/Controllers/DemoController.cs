d28f9c3db2b7d8a8cae40e60a0937fadd40fe911
thanhhn0902 <thanhhn0902@outlook.com>
2019-10-03 19:35:09 +0700

    sdvas

---
+++ ShopOnline/ShopOnline/Areas/Administrator/Controllers/DemoController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ShopOnline.Areas.Administrator.Controllers
+{
+    public class DemoController : Controller
+    {
+        // GET: Administrator/Demo
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
